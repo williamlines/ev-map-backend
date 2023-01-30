1226d6dac196d9c12fd703f5bab76ba044f5a3ab
William Lines <williamlines42@gmail.com>
2023-01-30 14:58:39 +0000

    added new packages

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+const dotenv = require("dotenv");
+dotenv.config();
+const mapBoxToken = process.env.MAPBOX_PUBLIC_TOKEN;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
